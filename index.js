e37581a5d42c6ef5d1d98fb97647e58c2f6d9634
Jorge del Casar <jorge.casar@gmail.com>
2017-02-24 14:56:34 +0100

    improved parser

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 'use estrict';
-
+const path = require('path');
+const fs = require('fs');
+const parser = require('./lib/parser');
 const options = require('command-line-args')(
   {
     name: 'input',
@@ -9,17 +11,33 @@ const options = require('command-line-args')(
   }
 );
 
-require('./lib/parser').parseFile(options.input)
-  .catch(function(error){
-    console.error(error);
-  })
-  .then(function(data){
-    const filename = options.input
-      .replace('data/', 'data_parsed/')
-      .replace('.in', '.json')
-    console.log(filename);
-    require('fs').writeFile(filename, JSON.stringify(data, null, 2), (err) => {
-      if (err) console.error(err);
-      console.log('It\'s saved!');
+function saveFile(inputFile, data) {
+  const pathParsed = path.parse(inputFile);
+  const dir = pathParsed.dir.replace('data', 'data_parsed');
+  if (!fs.existsSync(dir)){
+    fs.mkdirSync(dir);
+  }
+  const filename = path.join(dir, pathParsed.name + '.json');
+  data = JSON.stringify(data);
+  return new Promise(function (success, reject) {
+    fs.writeFile(filename, data, (err) => {
+      if (err) reject(err);
+      success(filename);
     });
   });
+}
+
+function parseInput(input) {
+  const time = new Date();
+  parser.parseFile(input)
+    .then(function(data) {
+      return saveFile(input, data);
+    })
+    .then(function(filename){
+      const processTime = new Date() - time;
+      console.log(`${filename} processed in ${processTime} ms.`);
+    })
+    .catch(console.error.bind(console));
+}
+
+parseInput(options.input);
